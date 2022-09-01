93f1e1c45585a727f940d36b10eebcbb7cdc1b54
Sultana Razia <sultanarazia483@gmail.com>
2022-09-01 09:35:46 +0600

    updated shipping form

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Shipment = () => {
+    const [user] = useAuthState(auth);
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [address, setAddress] = useState('');
@@ -10,10 +13,6 @@ const Shipment = () => {
 
     const handleNameBlur = event => {
         setName(event.target.value);
-    }
-
-    const handleEmailBlur = event => {
-        setEmail(event.target.value);
     }
 
     const handleAddressBlur = event => {
@@ -26,6 +25,8 @@ const Shipment = () => {
 
     const handleCreateUser = event => {
         event.preventDefault();
+        const shipping = { name, email, address, phone }
+        console.log(shipping);
     }
 
     return (
@@ -39,14 +40,14 @@ const Shipment = () => {
                     </div>
                     <div className="input-group">
                         <label htmlFor="email">Your Email</label>
-                        <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
+                        <input value={user?.email} readOnly type="email" name="email" id="" required />
                     </div>
                     <div className="input-group">
                         <label htmlFor="address">Address</label>
                         <input onBlur={handleAddressBlur} type="text" name="address" id="" required />
                     </div>
                     <div className="input-group">
-                        <label htmlFor="number">Phone Number</label>
+                        <label htmlFor="phone">Phone Number</label>
                         <input onBlur={handlePhoneBlur} type="number" name="phone" id="" required />
                     </div>
                     <p style={{ color: 'red' }}>{error}</p>
